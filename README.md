3dd47b91099afd052b4896aa2d16e8bc0906e016
Vicente Vendramin <guesser.vicente@gmail.com>
2023-02-17 11:50:58 -0300

    TrybeMail - complete application

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# TrybeMail
+# 🚀 TrybeMail
 Uma aplicação que renderiza uma lista de e-mails, sendo possível marcar um e-mail como lido ou não lido.
+
+
+Quando todos os e-mail da lista estiverem lidos, um `alert` deve ser exibido informando que todas mensagens foram lidas!
+
+### Descrição do projeto
+- Aplicação com apenas componentes funcionais;
+- Utilizado o hook `useState` como gerenciador de estado da aplicação;
+- Lista de e-mails salvos no estado da aplicação;
+- Hook `useEffect` para realizar mudanças na lista de e-mails salva no estado;
+- Sempre que todos os e-mails da lista forem marcados como "lidos", exiba um `alert` com a mensagem: "Parabéns! Você leu todas suas mensagens!".
---
+++ trybemail/src/App.js
@@ -0,0 +1,61 @@
+import { useEffect, useState } from 'react';
+
+import messagesList from './data/messagesList';
+import { READ, UNREAD } from './constants';
+
+import List from './components/List';
+import Controls from './components/Controls';
+import './App.css';
+
+function App() {
+  const [messages, setMessages] = useState(messagesList);
+
+  useEffect(() => {
+    const isAllMessagesRead = messages.every(({ status }) => status === READ);
+
+    if (isAllMessagesRead) {
+      alert('Parabéns! Você leu todas suas mensagens!');
+    }
+  }, [messages]);
+
+  const setMessageStatus = (messageId, newStatus) => {
+    const updateMessages = messages.map((currentMessage) => {
+      if (currentMessage.id === messageId) {
+        return { ...currentMessage, status: newStatus }
+      }
+      return currentMessage;
+    });
+
+    setMessages(updateMessages);
+  };
+
+  const markAllAsRead = () => {
+    const allAsRead = messages.map((message) => ({ ...message, status: READ }));
+    setMessages(allAsRead);
+  };
+
+  const markAllAsUnread = () => {
+    const markAllAsUnread = messages.map((message) => ({ ...message, status: UNREAD }));
+    setMessages(markAllAsUnread);
+  };
+
+  return (
+    <div className="App">
+      <header>
+        <h1>
+          TrybeMail
+        </h1>
+      </header>
+      <Controls
+        markAllAsRead={ markAllAsRead }
+        markAllAsUnread={ markAllAsUnread }
+      />
+      <List
+        messages={ messages }
+        setMessageStatus={ setMessageStatus }
+      />
+    </div>
+  );
+}
+
+export default App;
